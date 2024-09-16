76d56592d3c5bb2e95a357f8c190de41fcb81f40
Braden Allmond <braden.allmond@gmail.com>
2024-09-16 09:16:48 -0500

    correct VBF signal yield from scaling...

--- calculate_functions.py
+++ calculate_functions.py
@@ -40,7 +40,7 @@ def calculate_signal_background_ratio(data, backgrounds, signals):
     total_background += process_yield
   for signal in signals:
     signal_yield = np.sum(signals[signal]["BinnedEvents"])
-    if "VBF" in signal: signal_yield = signal_yield / 500.0 # VBF scaling # TODO: handle automatically
+    if "VBF" in signal: signal_yield = signal_yield / 100.0 # VBF scaling # TODO: handle automatically
     if "ggH" in signal: signal_yield = signal_yield / 100.0 # ggH scaling
     yields.append(signal_yield)
     total_signal += signal_yield
