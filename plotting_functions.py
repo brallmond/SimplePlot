a1206b491fccd0cf7acbff6602922af7d227196c
Naila Islam <148743519+nailaislam12@users.noreply.github.com>
2024-09-18 17:35:08 +0200

    Update plotting_functions.py
    small changes

--- plotting_functions.py
+++ plotting_functions.py
@@ -703,7 +703,7 @@ def set_vars_to_plot(final_state_mode, jet_mode="none"):
   '''
   vars_to_plot = ["HTT_m_vis", "HTT_dR", "HTT_pT_l1l2", #"FastMTT_PUPPIMET_mT", 
                   "FastMTT_mass",
-                  "PuppiMET_pt", "PuppiMET_phi", "PV_npvs", "HTT_mT_l1l2met_using_PUPPI_MET"]
+                  "PuppiMET_pt", "PuppiMET_phi", "PV_npvs"]
                   #"HTT_DiJet_MassInv_fromHighestMjj", "HTT_DiJet_dEta_fromHighestMjj"] 
                   # common to all final states # add Tau_decayMode
   FS_vars_to_add = final_state_vars[final_state_mode]
