77d1f07abb12996e11fd62beb8f44e1eaf8b6189
Naila Islam <148743519+nailaislam12@users.noreply.github.com>
2024-09-18 14:13:08 +0200

    Update FF_functions.py
    Including changes for emu

--- FF_functions.py
+++ FF_functions.py
@@ -4,6 +4,7 @@ from cut_ditau_functions import make_ditau_region, make_ditau_cut
 from cut_mutau_functions import make_mutau_region, make_mutau_cut
 from cut_etau_functions  import make_etau_region,  make_etau_cut
 from cut_dimuon_functions  import make_dimuon_region, make_dimuon_cut
+from cut_emu_functions  import make_emu_region, make_emu_cut
 from FF_dictionary import FF_fit_values, FF_mvis_weights
 from calculate_functions import user_exp, user_line
 from plotting_functions import set_vars_to_plot
@@ -58,6 +59,16 @@ def FF_control_flow(final_state_mode, semilep_mode, region, event_dictionary, De
   if (final_state_mode == "dimuon"):
     if (region == "SR"):      event_dictionary = make_dimuon_SR_cut(event_dictionary)
     else: print("missing region condition")
+
+  if (final_state_mode == "emu"):
+    if (region == "SR"):        event_dictionary   = make_emu_SR_cut(event_dictionary)
+    if (region == "AR"):        event_dictionary   = make_emu_AR_cut(event_dictionary)
+    if (region == "DRsr"):      event_dictionary   = make_emu_DRsr_cut(event_dictionary)
+    if (region == "DRar"):      event_dictionary   = make_emu_DRar_cut(event_dictionary)
+    if (region == "SR_aiso"):   event_dictionary   = make_emu_SR_aiso_cut(event_dictionary)
+    if (region == "AR_aiso"):   event_dictionary   = make_emu_AR_aiso_cut(event_dictionary)
+    if (region == "DRsr_aiso"): event_dictionary   = make_emu_DRsr_aiso_cut(event_dictionary)
+    if (region == "DRar_aiso"): event_dictionary   = make_emu_DRar_aiso_cut(event_dictionary)
 
   return event_dictionary
 
@@ -232,30 +243,75 @@ def make_dimuon_SR_cut(event_dictionary, iso_region=True):
   event_dictionary     = make_dimuon_region(event_dictionary, name, FS_pair_sign=-1)
   return event_dictionary
 
+#emu region cuts
+def make_emu_SR_cut(event_dictionary, iso_region_el=True, iso_region_mu=True):
+  name = "pass_SR_cuts" if ((iso_region_el == True) and (iso_region_mu ==True)) else "pass_SR_aiso_cuts"
+  event_dictionary   = make_emu_region(event_dictionary, name, 
+                         FS_pair_sign=-1, pass_el_iso_req=iso_region_el, el_iso_value=[0.00,0.15],
+                         pass_mu_iso_req=iso_region_mu, mu_iso_value=[0.00,0.15],
+                         pass_BTag_req=True)
+  print("passed SR")
+  return event_dictionary
+
+def make_emu_AR_cut(event_dictionary, iso_region_el=True, iso_region_mu=True):
+  name = "pass_AR_cuts" if ((iso_region_el == True) and (iso_region_mu ==True)) else "pass_AR_aiso_cuts"
+  event_dictionary = make_emu_region(event_dictionary, name, 
+                         FS_pair_sign=1, pass_el_iso_req=iso_region_el, el_iso_value=[0.00,0.15],
+                         pass_mu_iso_req=iso_region_mu, mu_iso_value=[0.00,0.15],
+                         pass_BTag_req=True)
+  print("passed AR")
+  return event_dictionary
+
+def make_emu_DRsr_cut(event_dictionary, iso_region_el=True, iso_region_mu=False):
+  name = "pass_DRsr_cuts" if ((iso_region_el == True) and (iso_region_mu == False)) else "pass_DRsr_aiso_cuts"
+  event_dictionary = make_emu_region(event_dictionary, name, 
+                         FS_pair_sign=-1, pass_el_iso_req=iso_region_el, el_iso_value=[0.00,0.15],
+                         pass_mu_iso_req=iso_region_mu, mu_iso_value=[0.00,0.15],
+                         pass_BTag_req=True)
+  print("passed DRsr")
+  return event_dictionary
+
+def make_emu_DRar_cut(event_dictionary, iso_region_el=True, iso_region_mu=False):
+  name = "pass_DRar_cuts" if ((iso_region_el == True) and (iso_region_mu == False)) else "pass_DRar_aiso_cuts"
+  event_dictionary = make_emu_region(event_dictionary, name, 
+                         FS_pair_sign=-1, pass_el_iso_req=iso_region_el, el_iso_value=[0.00,0.15],
+                         pass_mu_iso_req=iso_region_mu, mu_iso_value=[0.00,0.15],
+                         pass_BTag_req=True)
+  print("passed DRar")
+  return event_dictionary
+
+def make_emu_SR_aiso_cut(event_dictionary,):
+  return make_emu_SR_cut(event_dictionary, iso_region_el=False, iso_region_mu=True)
+
+def make_emu_AR_aiso_cut(event_dictionary):
+  return make_emu_AR_cut(event_dictionary, iso_region_el=False, iso_region_mu=True)
+
+def make_emu_DRsr_aiso_cut(event_dictionary):
+  return make_emu_DRsr_cut(event_dictionary, iso_region_el=False, iso_region_mu=False)
+
+def make_emu_DRar_aiso_cut(event_dictionary):
+  return make_emu_DRar_cut(event_dictionary, iso_region_el=False, iso_region_mu=False)
+
 #########################################################################################
 # Calculation Functions
 #########################################################################################
 
-def add_FF_weights(event_dictionary, final_state_mode, jet_mode, semilep_mode, closure=False, bypass=[]):
+def add_FF_weights(event_dictionary, final_state_mode, jet_mode, semilep_mode, closure=False, testing=True, bypass=[]):
   # interface to read FF_dictionary
   unpack_FF_vars = ["Lepton_pt", "HTT_m_vis", "l1_indices", "l2_indices"]
   unpack_FF_vars = (event_dictionary.get(key) for key in unpack_FF_vars)
   to_check = [range(len(event_dictionary["Lepton_pt"])), *unpack_FF_vars]
   FF_weights = []
+  jet_mode = jet_mode + "_testing" if (testing == True) else jet_mode
   QCD_fitvals   = FF_fit_values[final_state_mode][jet_mode]["QCD"]
   if (final_state_mode != "ditau"):
     WJ_fitvals   = FF_fit_values[final_state_mode][jet_mode]["WJ"]
   if bypass != []:  QCD_fitvals, WJ_fitvals = bypass, bypass
   for i, lep_pt, m_vis, l1_idx, l2_idx in zip(*to_check):
     m_vis = m_vis if m_vis < 300.0 else 299.0 # exactly 300 breaks index hack below
-    fakeleg_idx = l1_idx if final_state_mode == "ditau" else l2_idx # mutau/etau is always l2, ditau is always l1
-    lepleg_idx  = l2_idx if final_state_mode == "ditau" else l1_idx
-    tau_pt = lep_pt[fakeleg_idx]
-    low_val = 20.0 if final_state_mode == "ditau" else 30.0
-    #low_val = 40.0 if final_state_mode == "ditau" else 30.0
-    hi_val  = 140.0 if final_state_mode == "ditau" else 200
+    tau_pt = lep_pt[l2_idx] # "mu tau" means l1_idx is muon and l2_idx is tau
+    low_val = 40.0 if final_state_mode == "ditau" else 30.0
     tau_pt = tau_pt if tau_pt > low_val else low_val
-    tau_pt = tau_pt if tau_pt < hi_val else hi_val
     m_vis_idx = int(m_vis // 10) # hard-coding mvis bins of 10 GeV, starting at 0 and ending at 300 ( // is modulo division )
     f_QCD     = FF_mvis_weights[final_state_mode][jet_mode]["QCD"][m_vis_idx] if not closure else 1
     user_func = user_line if final_state_mode == "ditau" else user_exp
@@ -265,6 +321,7 @@ def add_FF_weights(event_dictionary, final_state_mode, jet_mode, semilep_mode, c
       FF_WJ      = user_func(tau_pt, *WJ_fitvals)
     else: pass
     if (semilep_mode == "Full"):
+    #if (full_FF == True):
       FF_weight = f_QCD * FF_QCD * 1.1 # OS/SS bias
       if (final_state_mode != "ditau"):
         FF_weight += f_WJ * FF_WJ
@@ -273,7 +330,7 @@ def add_FF_weights(event_dictionary, final_state_mode, jet_mode, semilep_mode, c
       elif (semilep_mode == "WJ"):   FF_weight = f_WJ  * FF_WJ
       else: print("add_FF_weights function error")
     if (FF_weight <= 0):
-      print("non-positive FF weights!")
+      print("negative FF weights!")
       print("FF_weight: ", FF_weight)
       print("tau_pt: ", tau_pt)
       print("value from fit: ", user_func(lep_pt[l1_idx], *QCD_fitvals))
@@ -284,21 +341,21 @@ def add_FF_weights(event_dictionary, final_state_mode, jet_mode, semilep_mode, c
 
 from producers import produce_FF_weight
 def set_JetFakes_process(setup, fakesLabel, semilep_mode):
-  # TODO could be improved by reducing variable name size and simplifying below operations
+  # could be improved by reducing name size and simplifying below operations
   JetFakes_dictionary = {}
-  _, final_state_mode, jet_mode, _, _ = setup.state_info
+  testing, final_state_mode, jet_mode, _, _ = setup.state_info
   _, _, _, do_JetFakes, _ = setup.misc_info
   vars_to_plot = set_vars_to_plot(final_state_mode, jet_mode)
   if (jet_mode != "Inclusive") and (do_JetFakes==True):
-    JetFakes_dictionary = produce_FF_weight(setup, jet_mode, semilep_mode)
+    JetFakes_dictionary = produce_FF_weight(setup, jet_mode)
   if (jet_mode == "Inclusive") and (do_JetFakes==True):
     fakesLabel = fakesLabel
     temp_JetFakes_dictionary = {}
     JetFakes_dictionary[fakesLabel] = {}
     JetFakes_dictionary[fakesLabel]["PlotEvents"] = {}
     JetFakes_dictionary[fakesLabel]["FF_weight"]  = {} 
-    jetCategories = ["0j", "1j", "GTE2j"] if final_state_mode == "ditau" else ["0j", "GTE1j"]
-    for internal_jet_mode in jetCategories:
+    for internal_jet_mode in ["0j", "GTE1j"]:
+      if testing: internal_jet_mode += "_testing"
       temp_JetFakes_dictionary[internal_jet_mode] = produce_FF_weight(setup, internal_jet_mode, semilep_mode)
       if ("0j" in internal_jet_mode):
         JetFakes_dictionary[fakesLabel]["FF_weight"]  = temp_JetFakes_dictionary[internal_jet_mode][fakesLabel]["FF_weight"]
