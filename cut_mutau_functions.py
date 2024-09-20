454c2170e28537f510726b32471b6122b2214f58
Braden Allmond <braden.allmond@gmail.com>
2024-09-20 10:53:10 -0500

    removed unused tag and probe code, fixed naming convention error in mutau

--- cut_mutau_functions.py
+++ cut_mutau_functions.py
@@ -40,20 +40,20 @@ def make_mutau_cut(event_dictionary, DeepTau_version, skip_DeepTau=False):
     if (tau_idx[l1_idx] != -1 and mu_idx[l2_idx] != -1):
       tauFSLoc = l1_idx
       tauBranchLoc = tau_idx[l1_idx]
-      muLoc  = l2_idx
+      muFSLoc  = l2_idx
       muBranchLoc = mu_idx[l2_idx]
     elif (tau_idx[l2_idx] != -1 and mu_idx[l1_idx] != -1):
       tauFSLoc = l2_idx
       tauBranchLoc = tau_idx[l2_idx]
-      muLoc  = l1_idx
+      muFSLoc  = l1_idx
       muBranchLoc = mu_idx[l1_idx]
     else:
       print("Should not print :)")
 
-    muPtVal    = lep_pt[muLoc] 
-    muEtaVal   = lep_eta[muLoc]
-    muPhiVal   = lep_phi[muLoc]
-    muIsoVal   = lep_iso[muLoc]
+    muPtVal    = lep_pt[muFSLoc] 
+    muEtaVal   = lep_eta[muFSLoc]
+    muPhiVal   = lep_phi[muFSLoc]
+    muIsoVal   = lep_iso[muFSLoc]
     muDxyVal   = abs(mu_dxy[muBranchLoc])
     muDzVal    = abs(mu_dz[muBranchLoc])
     muChgVal   = mu_chg[muBranchLoc]
@@ -195,148 +195,3 @@ def make_mutau_region(event_dictionary, new_branch_name, FS_pair_sign, pass_mu_i
     
   event_dictionary[new_branch_name] = np.array(pass_cuts)
   return event_dictionary
-
-def make_mutau_TnP_cut(event_dictionary, DeepTau_version, skip_DeepTau=False, requireProbe=False):
-  '''
-  Works similarly to 'make_ditau_cut'. 
-  Notably, the mutau cuts are more complicated, but it is simple to 
-  extend the existing methods as long as one can stomach the line breaks.
-  '''
-  nEvents_precut = len(event_dictionary["Lepton_pt"])
-  unpack_mutau = ["Lepton_pt", "Lepton_eta", "Lepton_phi", "Lepton_iso",
-                  "Muon_dxy", "Muon_dz", "Muon_charge", "Tau_dxy", "Tau_dz", "Tau_charge", "Tau_decayMode",
-                  "PuppiMET_pt", "PuppiMET_phi", "HTT_m_vis",
-                  "Lepton_tauIdx", "Lepton_muIdx", "l1_indices", "l2_indices",
-                  #"Tau_rawPNetVSjet", "Tau_rawPNetVSmu", "Tau_rawPNetVSe",
-                  "CleanJet_btagWP",
-                 ]
-  unpack_mutau = add_DeepTau_branches(unpack_mutau, DeepTau_version)
-  unpack_mutau = add_trigger_branches(unpack_mutau, final_state_mode="mutau")
-  unpack_mutau = (event_dictionary.get(key) for key in unpack_mutau)
-  to_check = [range(len(event_dictionary["Lepton_pt"])), *unpack_mutau] # "*" unpacks a tuple
-  FS_mu_pt, FS_mu_eta, FS_mu_phi, FS_mu_iso, FS_mu_dxy, FS_mu_dz, FS_mu_chg = [], [], [], [], [], [], []
-  FS_tau_pt, FS_tau_eta, FS_tau_phi, FS_tau_dxy, FS_tau_dz, FS_tau_chg, FS_tau_DM = [], [], [], [], [], [], []
-  pass_tag, pass_probe = [], []
-  pass_cuts, FS_mt, FS_nbJet, FS_acoplan = [], [], [], []
-  # note these are in the same order as the variables in the first line of this function :)
-  for i, lep_pt, lep_eta, lep_phi, lep_iso,\
-      mu_dxy, mu_dz, mu_chg, tau_dxy, tau_dz, tau_chg, tau_decayMode,\
-      MET_pt, MET_phi, mvis, tau_idx, mu_idx,\
-      l1_idx, l2_idx, btag,\
-      vJet, vMu, vEle, trg24mu, trg27mu, crosstrg in zip(*to_check):
-
-    # TODO: check if this can be removed
-    # some handling to figure out which FS index applies to what lepton
-    # note for the DeepTauID we use the tau branch index directly instead of the lepton branch
-    # (for tau branches we need the tau_idx, for lepton branches we can simply use the l1_idx, l2_idx)
-    tauFSLoc, tauBranchLoc, muFSLoc, muBranchLoc = 999, 999, 999, 999
-    if (tau_idx[l1_idx] != -1 and mu_idx[l2_idx] != -1):
-      tauFSLoc = l1_idx
-      tauBranchLoc = tau_idx[l1_idx]
-      muLoc  = l2_idx
-      muBranchLoc = mu_idx[l2_idx]
-    elif (tau_idx[l2_idx] != -1 and mu_idx[l1_idx] != -1):
-      tauFSLoc = l2_idx
-      tauBranchLoc = tau_idx[l2_idx]
-      muLoc  = l1_idx
-      muBranchLoc = mu_idx[l1_idx]
-    else:
-      print("Should not print :)")
-
-    muPtVal    = lep_pt[muLoc] 
-    muEtaVal   = lep_eta[muLoc]
-    muPhiVal   = lep_phi[muLoc]
-    muIsoVal   = lep_iso[muLoc]
-    muDxyVal   = abs(mu_dxy[muBranchLoc])
-    muDzVal    = abs(mu_dz[muBranchLoc])
-    muChgVal   = mu_chg[muBranchLoc]
-    tauPtVal   = lep_pt[tauFSLoc] 
-    tauEtaVal  = lep_eta[tauFSLoc]
-    tauPhiVal  = lep_phi[tauFSLoc]
-    tauDxyVal  = abs(tau_dxy[tauBranchLoc])
-    tauDzVal   = abs(tau_dz[tauBranchLoc])
-    tauChgVal  = tau_chg[tauBranchLoc]
-    mtVal      = calculate_mt(muPtVal, muPhiVal, MET_pt, MET_phi)
-    acoplanVal = calculate_acoplan(muPhiVal, tauPhiVal)
-
-    passZmassWindow = (40.0 < mvis < 80.0)
-
-    passTag   = trg24mu  # HLT_IsoMu24
-    passProbe = crosstrg # HLT_IsoMu20_eta2p1_LooseDeepTauPFTauHPS27_eta2p1_CrossL1
-
-    passTauPtAndEta = ((tauPtVal > 20.0) and (abs(tauEtaVal) < 2.1))
-    pass24MuPtAndEta  = (trg24mu and (muPtVal > 25.0) and (abs(muEtaVal < 2.1)))
-
-    skip_DMs = ((tau_decayMode[tauBranchLoc] != 5) and (tau_decayMode[tauBranchLoc] != 6))
-
-    #passTauDT  = (vJet[tauBranchLoc] >= 5)
-    passTauDTLep  = ((vMu[tauBranchLoc] >= 4) and (vEle[tauBranchLoc] >= 2))
-
-    #restrictTauDM = (tau_decayMode[tauBranchLoc] == 0)
-
-    pass_bTag = True
-    nbJet = 0
-    for value in btag:
-      if (value > 0): 
-        pass_bTag = False
-        nbJet += 1
-
-    # TODO : actually figure out what you want
-    # numerator shouldn't have tau pt restrictions, but denom should
-    # could duplicate data, run it through two different selections, then compare those
-    # sounds fine
-    if ( (passTauPtAndEta and pass24MuPtAndEta and passTauDTLep and passTag) and
-         (passZmassWindow and pass_bTag) ):
-    #if ((passMT and zmasswindow and passTag and (passOfflineTau and passOfflineMu) and skip_DMs and passTauDT) or
-    #    (requireProbe and 
-    #    (passMT and zmasswindow and passTag and passProbe and passOfflineMu and skip_DMs and passTauDT)) ):
-      pass_cuts.append(i)
-      FS_mu_pt.append(muPtVal)
-      FS_mu_eta.append(muEtaVal)
-      FS_mu_phi.append(muPhiVal)
-      FS_mu_iso.append(muIsoVal)
-      FS_mu_dxy.append(muDxyVal)
-      FS_mu_dz.append(muDzVal)
-      FS_mu_chg.append(muChgVal)
-
-      FS_tau_pt.append(tauPtVal)
-      FS_tau_eta.append(tauEtaVal)
-      FS_tau_phi.append(tauPhiVal)
-      FS_tau_dxy.append(tauDxyVal)
-      FS_tau_dz.append(tauDzVal)
-      FS_tau_chg.append(tauChgVal)
-      FS_tau_DM.append(tau_decayMode[tauBranchLoc])
-
-      FS_mt.append(mtVal)
-      FS_nbJet.append(nbJet)
-      FS_acoplan.append(acoplanVal)
-
-      pass_tag.append(passTag)
-      pass_probe.append(passProbe)
-
-  event_dictionary["pass_cuts"] = np.array(pass_cuts)
-  event_dictionary["FS_mu_pt"]  = np.array(FS_mu_pt)
-  event_dictionary["FS_mu_eta"] = np.array(FS_mu_eta)
-  event_dictionary["FS_mu_phi"] = np.array(FS_mu_phi)
-  event_dictionary["FS_mu_iso"] = np.array(FS_mu_iso)
-  event_dictionary["FS_mu_dxy"] = np.array(FS_mu_dxy)
-  event_dictionary["FS_mu_dz"]  = np.array(FS_mu_dz)
-  event_dictionary["FS_mu_chg"] = np.array(FS_mu_chg)
-  event_dictionary["FS_tau_pt"]  = np.array(FS_tau_pt)
-  event_dictionary["FS_tau_eta"] = np.array(FS_tau_eta)
-  event_dictionary["FS_tau_phi"] = np.array(FS_tau_phi)
-  event_dictionary["FS_tau_dxy"] = np.array(FS_tau_dxy)
-  event_dictionary["FS_tau_dz"]  = np.array(FS_tau_dz)
-  event_dictionary["FS_tau_chg"] = np.array(FS_tau_chg)
-  event_dictionary["FS_tau_DM"] = np.array(FS_tau_DM)
-  event_dictionary["FS_mt"]    = np.array(FS_mt)
-  event_dictionary["FS_nbJet"] = np.array(FS_nbJet)
-  event_dictionary["FS_acoplan"] = np.array(FS_acoplan)
-  event_dictionary["pass_tag"]   = np.array(pass_tag)
-  event_dictionary["pass_probe"] = np.array(pass_probe)
-  nEvents_postcut = len(np.array(pass_cuts))
-  print(f"nEvents before and after mutau tag and probe cuts = {nEvents_precut}, {nEvents_postcut}")
-  return event_dictionary
-
-
-
