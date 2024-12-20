f95b69420870a349c8846e0559df997b61599d9a
Braden Allmond <braden.allmond@gmail.com>
2024-12-20 15:38:12 -0600

    significant setup changes to simultaneously load V1 and V2 files. will shortly overwrite

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from sys import exit
-
 from file_map_dictionary   import testing_file_map, full_file_map, update_data_filemap
 from luminosity_dictionary import luminosities_with_normtag as luminosities
-
-from utility_functions       import make_directory, print_setup_info
+from utility_functions     import make_directory, print_setup_info
 
 class setup_handler:
   def __init__(self):
     import argparse 
     self.parser = argparse.ArgumentParser(description='Make a standard Data/MC agreement plot.')
-    # store_true : when the argument is supplied, store it's value as true
+    # What does store_true mean? It means when the argument is supplied, store it's value as true.
     self.parser.add_argument('--testing',      dest='testing',     default=False,       action='store_true')
     self.parser.add_argument('--final_state',  dest='final_state', default="mutau",     action='store')
     self.parser.add_argument('--jet_mode',     dest='jet_mode',    default="Inclusive", action='store')
@@ -24,10 +22,17 @@ class setup_handler:
     self.parser.add_argument('--presentation', dest='presentation_mode', default=False, action='store_true')
     self.parser.add_argument('--oneatatime',   dest='oneAtATime',  default=False,       action='store_true')
     self.parser.add_argument('--tau_pt',       dest='tau_pt_cut',  default="None",      action='store')
+    self.parser.add_argument('--temp_version', dest='temp_version', default="None",      action='store') # do not commit
+
 
     self.parser.add_argument('--one_process',    dest='one_process',    default=None,      action='store')
 
     args = self.parser.parse_args()
+    temp_version = args.temp_version # possible values are V1 and V2 # do not commit
+    print(f"HLEP FILE VERSION IS SET TO {temp_version}")
+    print()
+    print(f"IS YOUR SSD CONNECTED ?!")
+    print()
 
     # state info
     # final_state_mode affects good_events, datasets, plotting vars, etc. automatically
@@ -46,12 +51,11 @@ class setup_handler:
       exit()
 
     # file info
-    infile_directory = self.set_infile_directory(era, final_state_mode)
-    # for single process comparisons only
+    infile_directory = self.set_infile_directory(era, final_state_mode, temp_version)
     plot_dir_name = "FS_plots/" + args.plot_dir + "_" + final_state_mode + "_" + tau_pt_cut + "TauPtCategory_" + jet_mode
     plot_dir_name = make_directory(plot_dir_name, testing)
-    logfile       = open('outputfile.log', 'w') # could be improved, not super important right now
-    use_NLO       = args.use_NLO     # False by default, use LO DY if False
+    logfile       = open('outputfile.log', 'w')
+    use_NLO       = args.use_NLO     # True by default, use LO DY if False
     file_map      = self.set_file_map(testing, use_NLO, era)
     oneAtATime    = args.oneAtATime
 
@@ -68,7 +72,7 @@ class setup_handler:
       print(f"do_JetFakes = {do_JetFakes}, type: {type(do_JetFakes)}")
 
     semilep_mode = args.semilep_mode # default is QCD [possible values are Full, QCD, and WJ] (Full is both)
-    presentation_mode = args.presentation_mode # default is False, True hides yields and S/B on plots, and combines bkgds
+    presentation_mode = args.presentation_mode # default is False, True hides yields and combines minor backgrounds
 
     # comparison info (for file/process comparisons)
     one_process = args.one_process
@@ -77,24 +81,68 @@ class setup_handler:
     from collections import namedtuple
     state_info_template = namedtuple("State_info", "testing, final_state_mode, jet_mode, era, lumi, tau_pt_cut")
     self.state_info     = state_info_template(testing, final_state_mode, jet_mode, era, lumi, tau_pt_cut)
-    file_info_template  = namedtuple("File_info", "infile_directory, plot_dir_name, logfile, use_NLO, file_map, oneAtATime")
-    self.file_info      = file_info_template(infile_directory, plot_dir_name, logfile, use_NLO, file_map, oneAtATime)
+
+    file_info_template  = namedtuple("File_info", "infile_directory, plot_dir_name, logfile, use_NLO, file_map, oneAtATime, temp_version")
+    self.file_info      = file_info_template(infile_directory, plot_dir_name, logfile, use_NLO, file_map, oneAtATime, temp_version)
+
     misc_info_template  = namedtuple("Misc_info", "hide_plots, hide_yields, DeepTau_version, do_JetFakes, semilep_mode, one_process, presentation_mode")
     self.misc_info      = misc_info_template(hide_plots, hide_yields, DeepTau_version, do_JetFakes, semilep_mode, one_process, presentation_mode)
+
   # end class init
 
-  def set_infile_directory(self, era, final_state_mode):
+  def set_infile_directory(self, era, final_state_mode, temp_version):
     #lxplus_redirector = "root://cms-xrd-global.cern.ch//"
     #eos_dir           = "/eos/user/b/ballmond/NanoTauAnalysis/analysis/"
     era_modifier_2022 = "preEE" if (("C" in era) or ("D" in era)) else "postEE"
     era_modifier_2023 = "preBPIX" if ("C" in era) else "postBPIX"
     home_dir = "/Users/ballmond/LocalDesktop/HiggsTauTau" # there's no place like home :)
+    SSD_dir  = "/Volumes/IDrive/HTauTau_Data"
     #home_dir = "/Users/nailaislam/htt/new_samples/Hlep/2022postEE/"
-    #home_dir = "/Volumes/IDrive/HTauTau_Data/" # SSD hack
-    active_dir_2022 = "/V12_"+era_modifier_2022+"_HLepRareV2_notriggermatching/"
-    active_dir_2023 = "/HLep_2023"+era_modifier_2023+"/"
-    active_dir      = active_dir_2022 if ("2022" in era) else active_dir_2023
-    return home_dir + active_dir + final_state_mode # full directory path
+    #active_dir_2022 = "/V12_"+era_modifier_2022+"_HLepRareV2/"
+    #active_dir_2023 = "/HLep_2023"+era_modifier_2023+"/"
+
+    if   (temp_version == "V1"): 
+      active_dir = SSD_dir
+      if   (final_state_mode == "ditau"):
+        if   ("CD" in era):                     active_dir += "/HLepV1_2022preEE/"
+        elif ("EFG" in era):                    active_dir += "/HLepV1_2022postEE/"
+        elif ("C" in era) and ("D" not in era): active_dir += "/HLepV1_2023preBPIX/"
+        elif ("D" in era) and ("C" not in era): active_dir += "/HLepV1_2023postBPIX/"
+      elif (final_state_mode == "mutau"):
+        if   ("CD" in era):                     active_dir += "/HLepV1_2022preEE/"
+        elif ("EFG" in era):                    active_dir += "/HLepV1_2022postEE/"
+        elif ("C" in era) and ("D" not in era): active_dir += "/HLepV1_2023preBPIX/"
+        elif ("D" in era) and ("C" not in era): active_dir += "/HLepV1_2023postBPIX/"
+      else:
+        print(f"don't have files for final state : {final_state_mode}")
+    elif (temp_version == "V2"):
+      active_dir = home_dir
+      if   (final_state_mode == "ditau"):
+        if   ("CD" in era):                     active_dir += "/HLepV2_2022preEE/"
+        elif ("EFG" in era):                    active_dir += "/HLepV2_2022postEE/"
+        elif ("C" in era) and ("D" not in era): active_dir += "/HLepV2_2023preBPIX/"
+        elif ("D" in era) and ("C" not in era): active_dir += "/HLepV2_2023postBPIX/"
+      elif (final_state_mode == "mutau"):
+        if   ("CD" in era):
+          active_dir += "/HLepV2_2022preEE/"
+        elif ("EFG" in era):
+          actie_dir  =  SSD_dir
+          active_dir += "/HLepV2_2022postEE/"
+          print("actually, that's only on the SSD...")
+        elif ("C" in era) and ("D" not in era):
+          actie_dir  =  SSD_dir
+          active_dir += "/HLepV2_2023preBPIX/"
+          print("actually, that's only on the SSD...")
+        elif ("D" in era) and ("C" not in era):
+          active_dir += "/HLepV2_2023postBPIX/"
+      else:
+        print(f"don't have files for final state : {final_state_mode}")
+    else:
+      print("not set up for that")
+      print(era, temp_version, final_state_mode)
+      exit()
+    
+    return active_dir + final_state_mode
 
   
   def set_file_map(self, testing, use_NLO, era):
@@ -109,7 +157,7 @@ class setup_handler:
     return file_map
 
 
-def set_good_events(final_state_mode, era, non_SR_region=False, disable_triggers=False, useMiniIso=False):
+def set_good_events(final_state_mode, era, non_SR_region=False, temp_version="None", disable_triggers=False, useMiniIso=False):
   '''
   Return a string defining a 'good_events' flag used by uproot to preskim input events
   to only those passing these simple requirements. 'good_events' changes based on
@@ -144,8 +192,8 @@ def set_good_events(final_state_mode, era, non_SR_region=False, disable_triggers
   
   good_events =  "(METfilters) & (LeptonVeto==0)"
   jet_vetomaps = ""
-  if ("2023" in era) and any(affected_era in era for affected_era in ["D"]):
-    jet_vetomaps += " & (JetMapVeto_BPix_15GeV)"
+  #if ("2023" in era) and any(affected_era in era for affected_era in ["D"]):
+  #  jet_vetomaps += " & (JetMapVeto_BPix_15GeV)"
   if ("2022" in era) and any(affected_era in era for affected_era in ["E", "F", "G"]):
     jet_vetomaps += " & (JetMapVeto_EE_15GeV)"
   if (final_state_mode == "mutau"):
@@ -156,27 +204,33 @@ def set_good_events(final_state_mode, era, non_SR_region=False, disable_triggers
   good_events += " & (HTT_SRevent)" # preselected events from HTT ntuplizer
 
   if final_state_mode == "ditau":
-    triggers = "(HLT_DoubleMediumDeepTauPFTauHPS35_L2NN_eta2p1\
+    triggersV1 = "(HLT_DoubleMediumDeepTauPFTauHPS35_L2NN_eta2p1\
                | HLT_DoubleMediumDeepTauPFTauHPS30_L2NN_eta2p1_PFJet60\
                | HLT_DoubleMediumDeepTauPFTauHPS30_L2NN_eta2p1_PFJet75\
                | HLT_VBF_DoubleMediumDeepTauPFTauHPS20_eta2p1\
-               | HLT_DoublePFJets40_Mass500_MediumDeepTauPFTauHPS45_L2NN_MediumDeepTauPFTauHPS20_eta2p1)"
-    #triggers = "(HLT_DoubleMediumDeepTauPFTauHPS35_L2NN_eta2p1)"
+               | HLT_DoublePFJets40_Mass500_MediumDeepTauPFTauHPS45_L2NN_MediumDeepTauPFTauHPS20_eta2p1)" # HLepV1
+    if ("2022" in era):
+      triggersV2 = "((Trigger_ditau) | (Trigger_ditauplusjet) | (Trigger_VBFditau))"
+    else:
+      triggersV2 = "((Trigger_ditau) | (Trigger_ditauplusjet) | (Trigger_VBFditau) | (Trigger_VBFsingleTau))" # HLepV2 parking
 
+    triggers = triggersV1 if temp_version == "V1" else triggersV2
     good_events += " & (abs(HTT_pdgId)==15*15) & " + triggers
-    if disable_triggers: good_events = good_events.replace(" & (Trigger_ditau)", "")
 
   elif final_state_mode == "mutau":
-    good_events += " & (abs(HTT_pdgId)==13*15) & (Trigger_mutau)"
-    if disable_triggers: good_events = good_events.replace(" & (Trigger_mutau)", "")
+    good_eventsV1 = " & (abs(HTT_pdgId)==13*15) & (Trigger_mutau)" # HLepV1
+    good_eventsV2 = " & (abs(HTT_pdgId)==13*15) & ((Trigger_mutau) | (Trigger_SingleMuon))" # HLepV2
+    good_events += good_eventsV1 if temp_version == "V1" else good_eventsV2
 
   elif final_state_mode == "etau":
-    good_events += " & (abs(HTT_pdgId)==11*15) & (Trigger_etau)"
-    if disable_triggers: good_events = good_events.replace(" & (Trigger_etau)", "")
+    good_eventsV1 = " & (abs(HTT_pdgId)==11*15) & (Trigger_etau)" # HLepV1
+    good_eventsV2 = " & (abs(HTT_pdgId)==11*15) & ((Trigger_etau) | (Trigger_SingleElectron))" # HLepV2
+    good_events += good_eventsV1 if temp_version == "V1" else good_eventsV2
 
   elif final_state_mode == "emu":
-    good_events += " & (abs(HTT_pdgId)==11*13) & (Trigger_emu) "
-    if disable_triggers: good_events = good_events.replace(" & (Trigger_emu)", "")
+    good_eventsV1 = " & (abs(HTT_pdgId)==11*13) & (Trigger_emu) " # HLepV1
+    good_eventsV2 = " & (abs(HTT_pdgId)==11*13) & ((Trigger_emu) | (Trigger_mue))" # HLepV2
+    good_events += good_eventsV1 if temp_version == "V1" else good_eventsV2
 
   elif final_state_mode == "dimuon":
     # lepton veto must be applied manually for this final state
@@ -191,7 +245,7 @@ def set_good_events(final_state_mode, era, non_SR_region=False, disable_triggers
 if __name__ == "__main__":
   setup = setup_handler()
   testing, final_state_mode, jet_mode, era, lumi, tau_pt_cut = setup.state_info
-  infile_directory, plot_dir_name, logfile, use_NLO, file_map, one_file_at_a_time = setup.file_info
+  infile_directory, plot_dir_name, logfile, use_NLO, file_map, one_file_at_a_time, temp_version = setup.file_info
   hide_plots, hide_yields, DeepTau_version, do_JetFakes, semilep_mode, one_process, presentation_mode = setup.misc_info
 
   # test setup
