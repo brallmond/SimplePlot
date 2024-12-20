6934d890a49c39262c392e785f3eafdcf04a679b
Braden Allmond <braden.allmond@gmail.com>
2024-12-20 15:39:38 -0600

    propagated V1-V2 changes to producers

--- producers.py
+++ producers.py
@@ -11,33 +11,50 @@ import numpy as np
 import gc
 
 
-def set_AR_region(final_state_mode, era):
-  common_selection = set_good_events(final_state_mode, era, non_SR_region=True)
-  AR_region_ditau  = common_selection + " & (abs(HTT_pdgId)==15*15) & (Trigger_ditau | Trigger_ditauplusjet | Trigger_VBFditau)"
-  AR_region_mutau  = common_selection + " & (abs(HTT_pdgId)==13*15) & (Trigger_mutau)"
-  AR_region_etau   = common_selection + " & (abs(HTT_pdgId)==11*15) & (Trigger_etau)"
-  AR_region_emu    = common_selection + " & (abs(HTT_pdgId)==11*13) & (Trigger_emu)"
-  AR_region_dimuon = common_selection + " & (abs(HTT_pdgId)==13*13) & (HLT_IsoMu24)"
+def set_AR_region(final_state_mode, era, temp_version):
+  # TODO: need to abstract the handling in setup and then copy it over here
+  common_selection = set_good_events(final_state_mode, era, non_SR_region=True, temp_version=temp_version)
+  if final_state_mode == "ditau":
+    triggersV1 = "(HLT_DoubleMediumDeepTauPFTauHPS35_L2NN_eta2p1\
+               | HLT_DoubleMediumDeepTauPFTauHPS30_L2NN_eta2p1_PFJet60\
+               | HLT_DoubleMediumDeepTauPFTauHPS30_L2NN_eta2p1_PFJet75\
+               | HLT_VBF_DoubleMediumDeepTauPFTauHPS20_eta2p1\
+               | HLT_DoublePFJets40_Mass500_MediumDeepTauPFTauHPS45_L2NN_MediumDeepTauPFTauHPS20_eta2p1)" # HLepV1
+    triggersV2 = "((Trigger_ditau) | (Trigger_ditauplusjet) | (Trigger_VBFditau) | (Trigger_VBFsingleTau))" # HLepV2
 
-  AR_region_dictionary = {"ditau" : AR_region_ditau, "mutau" : AR_region_mutau, 
-                          "etau"  : AR_region_etau, "emu"    : AR_region_emu,
-                          "dimuon" : AR_region_dimuon}
-  AR_region = AR_region_dictionary[final_state_mode]
-  return AR_region
+    triggers = triggersV1 if temp_version == "V1" else triggersV2
+    common_selection += " & (abs(HTT_pdgId)==15*15) & " + triggers
+
+  elif final_state_mode == "mutau":
+    good_eventsV1 = " & (abs(HTT_pdgId)==13*15) & (Trigger_mutau)" # HLepV1
+    good_eventsV2 = " & (abs(HTT_pdgId)==13*15) & ((Trigger_mutau) | (Trigger_SingleMuon))" # HLepV2
+    common_selection += good_eventsV1 if temp_version == "V1" else good_eventsV2
+
+  elif final_state_mode == "etau":
+    good_eventsV1 = " & (abs(HTT_pdgId)==11*15) & (Trigger_etau)" # HLepV1
+    good_eventsV2 = " & (abs(HTT_pdgId)==11*15) & ((Trigger_etau) | (Trigger_SingleElectron))" # HLepV2
+    common_selection += good_eventsV1 if temp_version == "V1" else good_eventsV2
+
+  elif final_state_mode == "emu":
+    good_eventsV1 = " & (abs(HTT_pdgId)==11*13) & (Trigger_emu) " # HLepV1
+    good_eventsV2 = " & (abs(HTT_pdgId)==11*13) & ((Trigger_emu) | (Trigger_mue))" # HLepV2
+    common_selection += good_eventsV1 if temp_version == "V1" else good_eventsV2
+
+  return common_selection
 
 
 def produce_FF_weight(setup, fakesLabel, jet_mode, semilep_mode):
     # kinda weird, but okay
     testing, final_state_mode, _, era, lumi, tau_pt_cut = setup.state_info # don't reset jet_mode
-    using_directory, _, log_file, _, file_map, one_file_at_a_time = setup.file_info
+    using_directory, _, log_file, _, file_map, one_file_at_a_time, temp_version = setup.file_info
     _, _, DeepTau_version, _, _, _, _ = setup.misc_info
     if one_file_at_a_time: import glob
 
     jet_mode = jet_mode.removesuffix("_testing")
     dataset, _ = set_dataset_info(final_state_mode)
-    AR_region    = set_AR_region(final_state_mode, era) # same role as "set_good_events"
+    AR_region    = set_AR_region(final_state_mode, era, temp_version) # same role as "set_good_events"
     vars_to_plot = set_vars_to_plot(final_state_mode, jet_mode)
-    branches     = set_branches(final_state_mode, era, DeepTau_version, process=dataset)
+    branches     = set_branches(final_state_mode, era, DeepTau_version, process=dataset, temp_version=temp_version)
 
     FF_dictionary = {}
     FF_dictionary[fakesLabel] = {}
@@ -57,7 +74,7 @@ def produce_FF_weight(setup, fakesLabel, jet_mode, semilep_mode):
                                               branches, AR_region, final_state_mode,
                                               data=True, testing=testing)
       AR_events = AR_process_dictionary[dataset]["info"]
-      cut_events_AR = apply_AR_cut(dataset, AR_events, final_state_mode, jet_mode, semilep_mode, DeepTau_version, tau_pt_cut)
+      cut_events_AR = apply_AR_cut(era, dataset, AR_events, final_state_mode, jet_mode, semilep_mode, DeepTau_version, tau_pt_cut)
       if "FF_weight" not in FF_dictionary[fakesLabel]: # First file, or not doing one at a time
         FF_dictionary[fakesLabel]["FF_weight"]  = cut_events_AR["FF_weight"]
         for var in vars_to_plot:
